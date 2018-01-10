6fe57d3b693c7c79137b5fcd7e42d3712afc72f1
Ron Egli <ron@r-egli.com>
2018-01-10 18:46:05 +0000

    Updates

--- record.sh
+++ record.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-# RaspiDash
-# github.com/smugzombie
-
-# Define filename prefix 
+ 
 file_name=/opt/raspidash/video/webcam_
 
 for i in `seq 1 1 100` 
 do
-	# Grab current timestamp (Will be odd without rtc..)
-	current_time=$(date "+%b-%d-%Y__%H.%M.%S")	 
-	# Build the new filename
+   	echo "Welcome $i times..."
+
+	current_time=$(date "+%b-%d-%Y__%H.%M.%S")
+	echo "Current Time : $current_time"
+	 
 	new_fileName=$file_name$current_time.mp4
-	# Start recording to the new file
-	avconv -f video4linux2 -r 16 -s 640x480 -i /dev/video0 -c:v mpeg4 -r 16 -an -s 640x480 -b 1024k -t 00:30:30 -y $new_fileName
+	echo "New FileName: " "$new_fileName"
+	
+	#This was the toughest part of the whole project to find the optimal settings for recording.
+
+	# Normal Rotation
+	# avconv -f video4linux2 -r 16 -s 640x480 -i /dev/video0 -c:v mpeg4 -r 16 -an -s 640x480 -b 1024k -t 00:30:30 -y $new_fileName
+
+	# Rotated 180 Degrees
+	avconv -f video4linux2 -r 16 -s 640x480 -i /dev/video0 -c:v mpeg4 -r 16 -an -s 640x480 -b 1024k -t 00:30:30 -y -vf transpose=1,transpose=1 $new_fileName
+
+
+
+	echo "Done with recording..."
+
 done
  
